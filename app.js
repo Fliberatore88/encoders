45e3570b7623059eec14c4abe5bd6352df275938
Fliberatore88 <federico.liberatore88@gmail.com>
2022-01-16 01:59:11 -0300

    Se escribió el servidor en el puerto 3005 con todos el publicPath y todas las rutas de los html

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use (express.static(publicPath) );
+
+app.get ('/', (req,res) => {
+    res.sendFile (path.resolve (__dirname, 'views', 'index.html'));
+})
+
+app.get ('/Detalle-de-producto', (req,res) => {
+    res.sendFile (path.resolve(__dirname, 'views','productDetail.html'))
+})
+
+app.get ('/Carrito-de-compras', (req,res) => {
+    res.sendFile (path.resolve(__dirname, 'views','productCar.html'))
+})
+
+app.get ('/Registro', (req,res) => {
+    res.sendFile (path.resolve(__dirname, 'views','register.html'))
+})
+
+app.get ('/Login', (req,res) => {
+    res.sendFile (path.resolve(__dirname, 'views','login.html'))
+})
+
+
+
+app.listen(3005, (err) => {
+    if (err){
+        console.log('Error al levantar el servidor', err)
+    }else {
+        console.log('Levantó el Server correctamente')
+    }
+})
