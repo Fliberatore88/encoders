e036d5279a55d7853619231e5bc4dcf8172b36f9
Fliberatore88 <federico.liberatore88@gmail.com>
2022-03-13 07:07:32 -0300

    sacando algunos console.log

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -25,7 +25,7 @@ const productsController = {
     res.render ('./products/productCreateForm')
   },
   products: (req, res)=> {
-    console.log(req)
+
     res.render ('./products/products')
   }
 
