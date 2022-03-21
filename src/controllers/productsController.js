c749c320e408f1b58d9420d7c0a5f6b1dd6da8cc
Aurercys Arias <aurercys74@gmail.com>
2022-03-21 14:47:52 -0300

    detail

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,8 +2,8 @@ const productsController = {
   detail: (req,res) => {
     const product = {
     name: "Apple Macbook Pro",
-    price: 630.000,
-    discount: 1,
+    price:  630.000,
+    discount: 20,
     category: "Notebook",
     description: "La nueva MacBook Pro ofrece a los usuarios más pro un rendimiento revolucionario. Elige entre el chip M1 Pro o el aún más potente M1 Max para resolver las tareas profesionales más exigentes con una excepcional duración de la batería(1). Además, la MacBook Pro trae una espectacular pantalla Liquid Retina XDR de 14 pulgadas y puertos avanzados para sacarle más provecho que nunca",
     id: 1,
