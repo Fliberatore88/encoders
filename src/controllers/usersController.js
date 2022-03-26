8733a9cd36939afad77433a70b9e497c29aec635
Fliberatore88 <federico.liberatore88@gmail.com>
2022-03-26 04:16:56 -0300

    cookies

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -7,6 +7,7 @@ const usersController = {
 
   },
   register: (req,res) => {
+
     res.render('./users/register')
   },
   create: (req,res) => {
@@ -47,23 +48,22 @@ const usersController = {
 
   },
   login: (req,res) => {
-
+     
     res.render('./users/login')
   },
 
   enterLogin: (req,res) => {
       let userToLogin = User.findByField('email', req.body.email)
-   /* const errors = validationResult(req)
-    if (errors.isEmpty()){
-      res.render('./users/login', {msg: 'Mensaje todo OK',old: req.body})
-  } else {
-      res.render('./users/login', {errors: errors.mapped(), old: req.body})
-  }*/
+
     if(userToLogin) {
       let isPasswordOk = bcryptjs.compareSync(req.body.password, userToLogin.password)
       if (isPasswordOk){ 
         delete userToLogin.password
         req.session.userLogged = userToLogin;
+
+        if (req.body.remember_me) {
+          res.cookie('userEmail', req.body.email, {maxAge: (1000 * 60) * 2})
+        }
         return res.redirect('/users/userProfile')
       }
       return res.render('./users/login', { errors: {
@@ -83,12 +83,13 @@ const usersController = {
     })
   },
   profile: (req,res) => {
-    
+    console.log(req.cookies.userEmail)
     res.render('./users/userProfile',  {
       user: req.session.userLogged
     })
   },
   logout: (req,res) => {
+    res.clearCookie('userEmail')
       req.session.destroy();
       return res.redirect('/');
   },
